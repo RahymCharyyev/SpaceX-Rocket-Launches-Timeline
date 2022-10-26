661325d9c078e21f2421158c52164b5282c9506d
RahymCharyev <r569837@gmail.com>
2022-10-26 17:18:38 +0500

    ✨feature: Solved a bug with pop up

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getLaunches() {
   }
   let launches = launchesData.map((launch) => {
     const image = launch.links.patch.small || "./img/launch.jpg";
-    const details = launch.details || "";
+    const details = launch.details || "No details";
     return {
       name: launch.name,
       image,
@@ -26,20 +26,28 @@ const render = (launches) => {
   for (let i = 0; i < launches.length; i++) {
     document.querySelector(".launches__layout").innerHTML += `
     <div class="launches__layout-cont">
-      <div class="launches__layout-cont-block" onClick={myFunction(${i})}>
-          <img src="${launches[i].image}" alt="Image of Rocket Launch">
-          <p>${new Date(launches[i].date).toLocaleDateString()}</p>
-          <h2>${launches[i].name}</h2>
-          <p class="launch__title">${launches[i].details}</p>
-      </div>
-    </div>
+      <div class="launches__layout-cont-block" onClick={showDetails(${i})}>
+          <p class="launches__details" id="launches__details">${
+            launches[i].details
+          }</p>
+          <img src="${
+            launches[i].image
+          }" class="launches__img" alt="Image of Rocket Launch">
+          <p class="launches__date">${new Date(
+            launches[i].date
+          ).toLocaleDateString()}</p>
     `;
   }
 };
-function myFunction() {
-  document.querySelector(".launch__title").classList.toggle("visible");
-  console.log(document.querySelector(".launch__title"));
-}
+const showDetails = (nth) => {
+  let details = document.getElementsByClassName("launches__details");
+  for (i = 0; i < details.length; i++) {
+    if (i == nth) {
+      details[i].classList.toggle("launches__details-visible");
+    }
+  }
+};
+
 getLaunches().then((launches) => {
   render(launches);
 });
